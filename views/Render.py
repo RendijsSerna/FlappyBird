f74a150798321a14e666150a47becd1f3788bdae
s22sernrend <s22sernrend@venta.lv>
2024-02-09 15:05:40 +0200

    ADD: background floor and bird assets as well as blit to add them in render

--- views/Render.py
+++ views/Render.py
@@ -6,10 +6,20 @@ class Renderer:
         self.width = 400
         self.height = 600
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.background = pygame.image.load("assets/images/background-day.png")
+        self.base = pygame.image.load("assets/images/base.png")
+        self.bird = pygame.image.load("assets/images/yellowbird-downflap.png")
 
     def render(self, bird, pipes):
         self.screen.fill((0, 0, 0))
-        pygame.draw.rect(self.screen, (255, 255, 255), (bird.x, bird.y, 30, 30))
+        self.background = pygame.transform.scale(self.background, (self.width, self.height))
+        self.base = pygame.transform.scale(self.base, (self.width, 200))
+
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.base, (0, 570))
+        self.screen.blit(self.bird, (bird.x, bird.y))
+
+
 
         for pipe in pipes:
             pygame.draw.rect(self.screen, (0, 255, 0), (pipe.x, pipe.y, pipe.width, pipe.height))
